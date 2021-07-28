37a3e4f910130e84591e3c23644d84a7d1ca6e5d
MR0100 <mr.vaghasiya197@gmail.com>
2021-07-28 14:49:47 +0530

    check msg

--- src/components/TrackChartComp/TrackChartComp.js
+++ src/components/TrackChartComp/TrackChartComp.js
@@ -1,12 +1,34 @@
 import './TrackChartComp.css';
 const TrackChartComp = () => {
+    const trackChartData = [{ month: "Jan", amt: 100, },
+        { month: "Feb", amt: 300, },
+        { month: "Mar", amt: 50, },
+        { month: "Apr", amt: 500, },
+        { month: "May", amt: 600, },
+        { month: "Jun", amt: 200, },
+        { month: "Jul", amt: 800, },
+        { month: "Aug", amt: 600, },
+        { month: "Sep", amt: 300, },
+        { month: "Oct", amt: 200, },
+        { month: "Nov", amt: 900, },
+        { month: "Dec", amt: 1000, }];
+    var high = 1000;
+    var low = 100;
+    var points = [ high / 3 ,high / 2 ,high / 1.5];
+    
     return (
         <div className="TrackChartComp">
-            <br /><br /><br /><br /><br /><br />brbrbr
-            <br /><br /><br /><br /><br /><br />br
-            <br /><br /><br />rb
-            rbr
-            b
+            <div>
+                {trackChartData.map((e) => {
+                    return (<div style={
+                        e.amt >= points[0] && e.amt <= points[1] ? { color: "red" } : e.amt >= points[1] && e.amt <= points[2] ? { color: "orange" } : e.amt >= points[2] ? { color: "green" } : {color: "red"}                    }>
+                        { }
+                    </div>);
+                    
+                })}
+                asdasdasd
+                    asdasdasd
+            </div>
         </div>
     );
 };
